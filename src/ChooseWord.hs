410430e99dbe732a40ad9bb3e70ff2f5f2b50dd4
jackesfonseca <jackesdafonseca@gmail.com>
2024-11-05 18:33:24 -0300

    adicionando função que gera palavras pseudoaleatorias

--- src/ChooseWord.hs
+++ src/ChooseWord.hs
@@ -1,4 +1,19 @@
 module ChooseWord (chooseWord) where
 
+import System.Random (mkStdGen, randomR)
+
+-- Lista de palavras pré-definida
+palavras :: [String]
+palavras = ["haskell", "programacao", "computador", "jogo", "desafio"]
+
+-- Função para escolher uma palavra aleatória da lista de palavras
+-- Retorna um `String` puro (determinístico) sem `IO`
 chooseWord :: String
-chooseWord = "haskell"
+chooseWord = 
+    let
+        -- Usa uma semente fixa para o gerador
+        gerador = mkStdGen 42
+        -- Gera um índice aleatório dentro do intervalo da lista de palavras
+        (indiceAleatorio, _) = randomR (0, length palavras - 1) gerador
+    in palavras !! indiceAleatorio
+
