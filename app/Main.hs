d47946d7f99ecd568d2426d737694782e27d1e9b
vnsrz <viniciusroriz21@gmail.com>
2024-11-06 16:09:02 -0300

    simplifica a implementação de IO

--- app/Main.hs
+++ app/Main.hs
@@ -7,19 +7,17 @@ main :: IO ()
 main = do
   -- O operador '<-' transforma o 'IO State' retornado pelo newGame em 'State'
   initialState <- newGame
-  selectedWord <- word initialState
 
-  putStrLn $ "Palavra: " ++ selectedWord ++ "\n"
+  putStrLn $ "Palavra: " ++ word initialState ++ "\n"
 
   -- Primeiro palpite
-  -- O 'return' transforma o 'State' em 'IO State'
-  updatedState1 <- makeGuess 'h' (return initialState) 
-  putStrLn "Palpite: h"
+  let updatedState1 = makeGuess 'o' initialState 
+  putStrLn "Palpite: o"
   putStrLn $ "Letras usadas: " ++ show (guessedLetters updatedState1)
   putStrLn $ "Vidas restantes: " ++ show (livesRemaining updatedState1) ++ "\n"
 
   -- Segundo palpite
-  updatedState2 <- makeGuess 'j' (return updatedState1)
+  let updatedState2 = makeGuess 'j'  updatedState1
   putStrLn "Palpite: j"
   putStrLn $ "Letras usadas: " ++ show (guessedLetters updatedState2)
   putStrLn $ "Vidas restantes: " ++ show (livesRemaining updatedState2)
--- src/GameState.hs
+++ src/GameState.hs
@@ -3,11 +3,13 @@ module GameState (State(..), newGame) where
 import ChooseWord (chooseWord)
 
 data State = State {
-    word :: IO String,
+    word :: String,
     guessedLetters :: [Char],
     livesRemaining :: Int
 }
 
 newGame :: IO State
 newGame = do -- IO precisa estar dentro de um 'do'
-    return (State chooseWord [] 6)
+    -- Transforma a IO String em String
+    selectedWord <- chooseWord
+    return (State selectedWord [] 6)
