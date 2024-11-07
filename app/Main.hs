beae2e038e92c8617bb16cdc3638ccf475138877
vnsrz <viniciusroriz21@gmail.com>
2024-11-07 10:34:25 -0300

    Altera a formatação

--- app/Main.hs
+++ app/Main.hs
@@ -16,14 +16,21 @@ main = do
   let updatedState1 = makeGuess 'o' initialState 
   renderState updatedState1
 
-  putStrLn "Palpite: o"
-  putStrLn $ "Letras usadas: " ++ show (guessedLetters updatedState1)
-  putStrLn $ "Vidas restantes: " ++ show (livesRemaining updatedState1) ++ "\n"
-
   -- Segundo palpite
-  let updatedState2 = makeGuess 'j'  updatedState1
+  let updatedState2 = makeGuess 'z'  updatedState1
   renderState updatedState2
 
-  putStrLn "Palpite: j"
-  putStrLn $ "Letras usadas: " ++ show (guessedLetters updatedState2)
-  putStrLn $ "Vidas restantes: " ++ show (livesRemaining updatedState2)
+  -- let updatedState3 = makeGuess 'q'  updatedState2
+  -- renderState updatedState3
+
+  -- let updatedState4 = makeGuess 'w'  updatedState3
+  -- renderState updatedState4
+
+  -- let updatedState5 = makeGuess 'k'  updatedState4
+  -- renderState updatedState5
+
+  -- let updatedState6 = makeGuess 'y'  updatedState5
+  -- renderState updatedState6
+
+  -- let updatedState7 = makeGuess 'x'  updatedState6
+  -- renderState updatedState7
--- src/RenderState.hs
+++ src/RenderState.hs
@@ -1,6 +1,7 @@
 module RenderState (renderState) where
 
 import GameState (State (..))
+import Data.List ( intercalate )
 
 hangmanPics :: [String]
 hangmanPics = [
@@ -16,14 +17,24 @@ hangmanPics = [
 displayWord :: String -> [Char] -> String
 displayWord w guessed = concatMap (\c -> if c `elem` guessed then [c, ' '] else "_ ") w
 
+displayGuessed :: State -> String
+displayGuessed state = 
+    "Letras usadas: " ++ formatGuessedLetters (guessedLetters state)
+
+formatGuessedLetters :: [Char] -> String
+formatGuessedLetters letters = 
+    intercalate ", " (map (:[]) letters)
+
 renderState :: State -> IO ()
 renderState state = do
     let totalLives = 6
         index = totalLives - livesRemaining state
         hangmanPic = hangmanPics !! index
         wordDisplay = displayWord (word state) (guessedLetters state)
-    putStrLn hangmanPic
-    putStrLn ""
+        lettersDisplay = displayGuessed state 
+    putStrLn "\n"
+    putStr hangmanPic
+    putStr " "
     putStrLn wordDisplay
-    putStrLn ""
+    putStrLn lettersDisplay
 
