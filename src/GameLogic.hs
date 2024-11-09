6e25aa9a184393b4bf836c5f1491d103184d3cd3
vnsrz <viniciusroriz21@gmail.com>
2024-11-09 15:22:51 -0300

    move a função para o módulo GameLogic e altera o retorno para Int

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -1,4 +1,4 @@
-module GameLogic (makeGuess) where
+module GameLogic (makeGuess, checkResult) where
 
 import GameState (State (..))
 
@@ -10,3 +10,9 @@ makeGuess letter gameState =
         guessedLetters = letter : guessedLetters gameState,
         livesRemaining = livesRemaining gameState - 1
     }
+
+checkResult :: State -> Int
+checkResult state
+    | all (`elem` guessedLetters state) (word state) = 3 -- Vitória
+    | livesRemaining state <= 0                      = 2 -- Derrota
+    | otherwise                                      = 1 -- Continua
--- src/GameResult.hs
+++
@@ -1,9 +0,0 @@
-module GameResult (checkResult) where
-
-import GameState (State (..))
-
-checkResult :: State -> Bool
-checkResult state
-    | all (`elem` guessedLetters state) (word state) = True 
-    | livesRemaining state <= 0                       = False 
-    | otherwise                                       = False 
