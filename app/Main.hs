35bb5fa00fad86cc23734da46b9611295d75d7bb
vnsrz <viniciusroriz21@gmail.com>
2024-11-09 15:27:43 -0300

    Valida os palpites do usuário e implementa a vitória/derrota no loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,43 @@
 module Main where
 
-import Control.Monad (forever)
-import System.Exit (exitSuccess)
+import Control.Monad ()
+import System.Exit ()
 
-import GameLogic (makeGuess)
+import GameLogic (makeGuess, checkResult, validateGuess)
 import GameState (State (..), newGame)
 import RenderState (renderState)
 
 main :: IO ()
 main = do
-  -- Inicializa o jogo com o estado inicial
-  initialState <- newGame
+    -- Inicializa o jogo com o estado inicial
+    initialState <- newGame
 
-  -- Palavra escolhida aleatoriamente 
-  putStrLn $ "Palavra: " ++ word initialState ++ "\n"
+    -- Palavra escolhida aleatoriamente 
+    putStrLn $ "Palavra: " ++ word initialState ++ "\n"
 
-  -- Renderiza o estado inicial do jogo
-  renderState initialState
+    -- Renderiza o estado inicial do jogo
+    renderState initialState
 
-  -- Chama a função de loop com o estado inicial
-  gameLoop initialState
+    -- Chama a função de loop com o estado inicial
+    gameLoop initialState
 
 -- Função recursiva que mantém o estado atualizado em cada iteração
 gameLoop :: State -> IO ()
 gameLoop currentState = do
-  putStrLn "Digite um único caractere:"
-  guess <- getChar
-
-  -- Atualiza o estado com o palpite
-  let updatedState = makeGuess guess currentState
-  renderState updatedState
-
-  -- A fazer: Condição de parada -> se jogador perdeu ou ganhou, sai do jogo
-  -- Exemplo condição de parada:
-    -- if guess == 'w'
-    -- then exitSuccess
-    -- else gameLoop updatedState
+    putStrLn "Digite um único caractere:"
+    guess <- getChar
+    _ <- getLine -- Remove o \n que é gerado ao apertar Enter
+    
+    -- Checa se o caractere é válido
+    isValidGuess <- validateGuess guess currentState
+    if isValidGuess
+        then do 
+            let updatedState = makeGuess guess currentState -- Atualiza o estado com o palpite
+            renderState updatedState
+            let result = checkResult updatedState
+            if result == 1
+                then gameLoop updatedState
+                else if result == 2
+                    then putStrLn $ "\n\nVocê Perdeu. A palavra era: " ++ word currentState ++ "\n"
+                    else putStrLn "\n\nVocê Ganhou. Parabéns!"
+        else gameLoop currentState
